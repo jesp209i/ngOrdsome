48b722837f96759fb9ad7f7e44c1fea27447cbe7
Jesper Madsen <jesp209i@edu.eal.dk>
2019-04-28 17:54:14 +0200

    wip

--- src/app/answer-detail/answer-detail.component.ts
+++ src/app/answer-detail/answer-detail.component.ts
@@ -1,16 +1,27 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { Request } from '../model/request';
+import {ApiService} from '../service/api.service';
 
 @Component({
   selector: 'app-answer-detail',
   templateUrl: './answer-detail.component.html',
   styleUrls: ['./answer-detail.component.css']
 })
-export class AnswerDetailComponent implements OnInit {
+export class AnswerDetailComponent implements OnInit, OnChanges {
   @Input() request: Request;
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.request) {
+      this.getAnswers();
+    }
+  }
+
+  private getAnswers() {
+    this.apiService.getAnswers(this.request.requestId)
+      .subscribe(answers => this.request.answers = answers);
+  }
 }
